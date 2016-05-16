f4da80ef8e772ee5cc197e8840467529ffc499a6
LBatters <mtemad@mail.ru>
2016-05-16 18:49:28 +0300

    (Черновик)Первая версия поиска по id.

--- nf6_script/artem.sql
+++ nf6_script/artem.sql
@@ -1,50 +1,33 @@
-SELECT nam, fam, otc
+SELECT fam, rost
                 FROM
-                    --Таблцица с nam
-                    (select a.id_stud,nam
-                       from (
-                               select id_stud, dat,nam, abs(dat -d) as div
-                               from stud_nam
-                            ) as a
-                       inner join
-                            (
-                             select id_stud, min( abs(d-dat)) as mindiv
-                             from stud_nam
-                             where dat <= d
-                             group by id_stud
-                            ) as b
-                       on a.id_stud= b.id_stud and a.div = b.mindiv) as nam
-                    inner join
                     --Таблцица с fam
                     (select a.id_stud,fam
                        from (
                                select id_stud, dat,fam, abs(dat -d) as div
-                               from  stud_fam
+                               from stud_fam
                             ) as a
                        inner join
                             (
                              select id_stud, min( abs(d-dat)) as mindiv
-                             from  stud_fam
+                             from stud_fam
                              where dat <= d
                              group by id_stud
                             ) as b
                        on a.id_stud= b.id_stud and a.div = b.mindiv) as fam
-                    on nam.id_stud = fam.id_stud
-                       inner join
-                       
-                    --Таблцица с otc
-                    (select a.id_stud,otc
+                    inner join
+                    --Таблцица с rost
+                    (select a.id_stud,rost
                        from (
-                               select id_stud, dat,otc, abs(dat -d) as div
-                               from  stud_otc
+                               select id_stud, dat,rost, abs(dat -d) as div
+                               from  stud_rost
                             ) as a
                        inner join
                             (
                              select id_stud, min( abs(d-dat)) as mindiv
-                             from  stud_otc
+                             from  stud_rost
                              where dat <= d
                              group by id_stud
                             ) as b
-                       on a.id_stud= b.id_stud and a.div = b.mindiv) as otc
-                    on fam.id_stud = otc.id_stud;
+                       on a.id_stud= b.id_stud and a.div = b.mindiv) as rost
+                    on fam.id_stud = rost.id_stud;
                            
